4a905390a01c597892bee5142b3fb70cdc7c54b1
Akshay Rao <mail2akshayrao@gmail.com>
2024-10-09 16:39:50 +0530

    Added home route

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask app!"
+
+if __name__ == '__main__':
+    app.run()
+
 
 @app.route('/ask', methods=['POST'])
 def ask():
